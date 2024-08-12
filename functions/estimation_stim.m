70f76318274ef303394c9d7cc344070981318bfc
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-08-12 10:22:14 +0200

    Delete functions directory

--- functions/estimation_stim.m
+++
@@ -1,9 +0,0 @@
-function estimation_stim(output_path)
-
-file_dir = strcat(output_path, '\SPM.mat');
-
-matlabbatch{1}.spm.stats.fmri_est.spmmat = {file_dir};
-matlabbatch{1}.spm.stats.fmri_est.write_residuals = 0;
-matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
-
-spm_jobman('run', matlabbatch);
