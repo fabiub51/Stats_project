fa6949728d4de4ba810c663073acb95be3847417
fabiub51 <154235429+fabiub51@users.noreply.github.com>
2024-08-12 10:17:41 +0200

    Delete DCM directory

--- DCM/functions/dcm_estimation.m
+++
@@ -1,9 +0,0 @@
-function dcm_estimation(data_path, dcm_models)
-
-matlabbatch{1}.spm.dcm.fmri.estimate.dcmmat = {...
-    fullfile(data_path, dcm_models{1}); ...
-    fullfile(data_path, dcm_models{2});
-    fullfile(data_path, dcm_models{3})};
-
-% Run and save the results
-spm_jobman('run', matlabbatch);
