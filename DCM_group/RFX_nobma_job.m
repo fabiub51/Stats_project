9ab148f81a319fd9e0ba46b9b230c6556893ee74
lmiln <110472839+lmiln@users.noreply.github.com>
2024-08-12 16:53:05 +0200

    Add files via upload

---
+++ DCM_group/RFX_nobma_job.m
@@ -0,0 +1,13 @@
+%-----------------------------------------------------------------------
+% Job saved on 08-Aug-2024 19:40:36 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.dcm.bms.inference.dir = {'/Users/almila/Desktop/stats project copy 4/groupleveldcm'};
+matlabbatch{1}.spm.dcm.bms.inference.sess_dcm = {};
+matlabbatch{1}.spm.dcm.bms.inference.model_sp = {'/Users/almila/Desktop/stats project copy 4/bms1/model_space1.mat'};
+matlabbatch{1}.spm.dcm.bms.inference.load_f = {''};
+matlabbatch{1}.spm.dcm.bms.inference.method = 'RFX';
+matlabbatch{1}.spm.dcm.bms.inference.family_level.family_file = {''};
+matlabbatch{1}.spm.dcm.bms.inference.bma.bma_no = 0;
+matlabbatch{1}.spm.dcm.bms.inference.verify_id = 1;
